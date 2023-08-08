72efafe3eb55fd32db96b96f88a4068520506ae0
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-08-08 23:01:22 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,53 @@
+public class Demo1{
+    public static void main(String[] args) {
+         /* 
+            Single Loop, Nested Loops
+            Via +
+
+            + + + + +
+            + + + +
+            + + +
+            + +
+            +                           
+            + +
+            + + +
+            + + + +
+            + + + + +
+         */
+
+
+            int y=0;
+            int z=5;
+            while (y++ < 5){
+            int x = 0;
+            while (x< z){
+                System.out.print("+ ");
+                x++;
+            }
+            System.out.println();
+            z--;
+    }
+            
+            int a=1;
+
+            while(a<=5)
+            {
+
+                int k=1;
+                while(k<=a){
+
+                System.out.print("+ ");
+                k++;
+
+            }
+            System.out.println();
+            a++;
+
+            }
+
+           
+            
+
+        }
+    }
+    

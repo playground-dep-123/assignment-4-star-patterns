0ae7b6f2f1ac8db9cabb7f640abd68ea033ce6f5
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-08-09 09:44:06 +0530

    Complete Task 5

--- Demo5.java
+++ Demo5.java
@@ -11,5 +11,40 @@ public class Demo5 {
             + + + + +
 
            */
+
+           int row=5;
+           int i=1;
+
+           while(i<=5)
+           {
+            int x=1;
+            while(x<=row)
+            {
+                if(i==2 && (x==2||x==4))
+                {
+
+                    if(x==2) System.out.print("/ ");
+                    else System.out.print("\\ ");
+                   x++;
+                   
+                   
+                }
+
+                 if(i==4 && (x==2||x==4))
+                {
+
+                    if(x==2) System.out.print("\\ ");
+                    else System.out.print("/ ");
+                   x++;
+                   
+                   
+                }
+                System.out.print("+ ");
+                x++;
+            }
+            System.out.println();
+            i++;
+           }
+
     }
 }
